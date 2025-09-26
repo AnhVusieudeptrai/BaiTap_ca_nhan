0ade0a44d4a9a0bdebfaa572d78487cd9589af0e
LENOVO <anhvuu1804@gmail.com>
2025-09-26 10:14:46 +0700

    Hoàn thành màn hình 1.(LoginActivity)

---
+++ Manhinhungdung_AnhVu_2348/app/src/main/java/com/example/my_app/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.example.my_app;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText edtUser, edtPass;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        edtUser = findViewById(R.id.edtUser);
+        edtPass = findViewById(R.id.edtPass);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = edtUser.getText().toString().trim();
+                String pass = edtPass.getText().toString().trim();
+
+                if (user.equals("admin") && pass.equals("123")) {
+                    Intent i = new Intent(LoginActivity.this, MainMenu_Activity.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Sai tài khoản hoặc mật khẩu!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
